6a8c2e912cedb094e2f27a7bcbd9b4336780bf47
Yan He <yan.he@baisonmail.com>
2018-06-30 14:00:37 +0800

    (fix:component) Fixed import ui.router error.

--- generators/component/templates/module.ts
+++ generators/component/templates/module.ts
@@ -1,11 +1,10 @@
 import * as angular from 'angular';
-import uiRouter from '@uirouter/angularjs';
 
 import { <%= className %>Component } from '.<%= file %>.component';
 
 export const <%= className %>Module = angular
   .module('<%= name %>', [
-    uiRouter
+    'ui.router'
   ])
   .component('<%= name %>', <%= className %>Component)
   .config($stateProvider => {
